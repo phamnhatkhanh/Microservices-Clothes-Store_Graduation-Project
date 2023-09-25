7086720d9e7ee11678835e8fb0e3bcd5537a4587
phamnhatkhanh <khanhhcm4@gmail.com>
2023-09-25 15:20:34 +0700

    Project: setting packages and config for services

--- discovery-service/src/main/java/com/clothesstore/discoveryservice/SecurityConfig.java
+++ discovery-service/src/main/java/com/clothesstore/discoveryservice/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.ecommerce.discoveryservice;
+package com.clothesstore.discoveryservice;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
---
+++ inventory-service/src/test/java/com/clothesstore/inventoryservice/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clothesstore.inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
