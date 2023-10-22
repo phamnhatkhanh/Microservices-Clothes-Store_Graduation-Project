7a16e12b1b5ba4fb44ea57c6773a25da79e3f726
phamnhatkhanh <khanhhcm4@gmail.com>
2023-10-22 23:02:32 +0700

    Project: setup eureka and api gateway

--- order-service/src/main/java/com/clothesstore/orderservice/config/WebClientConfig.java
+++ order-service/src/main/java/com/clothesstore/orderservice/config/WebClientConfig.java
@@ -1,6 +1,6 @@
 package com.clothesstore.orderservice.config;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
